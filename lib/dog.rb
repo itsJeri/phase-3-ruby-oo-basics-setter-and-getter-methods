5e40650e922f568645161f046c372bfd80c5b635
Jerry Tong <zihangtong@gmail.com>
2022-01-11 11:18:46 -0500

    commit tree bd6ee383bfc18ab8cc434cf1a94c32d81a0c0fa1

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,17 @@
 class Dog
+    def name=(puppy)
+        @name = puppy
+    end
 
+    def name
+        @name
+    end
+
+    def breed=(breed)
+        @breed = breed
+    end
+
+    def breed
+        @breed
+    end
 end
